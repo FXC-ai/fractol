c86615cf01ff3e5b5d2a45d440d89a213c876c80
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-02-09 17:02:04 +0100

    square able to move in the window

--- main.c
+++ main.c
@@ -23,8 +23,8 @@ typedef struct s_img
 
 typedef struct s_datas
 {
-    t_vars  vars;
-    t_img   img;
+    t_vars  *vars;
+    t_img   *img;
 
     int     *color;
     int     *x;
@@ -61,7 +61,7 @@ int fx_kboard_hook(int key_code, t_datas *datas)
 
     if (key_code == 53)
     {
-        mlx_destroy_window(datas->vars.mlx, datas->vars.win);
+        mlx_destroy_window(datas->vars->mlx, datas->vars->win);
         exit(0);
     }
     else if (key_code == 0)
@@ -70,15 +70,25 @@ int fx_kboard_hook(int key_code, t_datas *datas)
     }
     else if (key_code == 12)
     {
-        mlx_mouse_get_pos(datas->vars.win , &x, &y);
+        mlx_mouse_get_pos(datas->vars->win , &x, &y);
         printf("x= %d et y= %d\n", x, y);
+    }
+    else if (key_code == 125)
+    {
+        *(datas->y) += 50;
     }
     else if (key_code == 126)
     {
-        *(datas->color) = create_trgb(0,0,255,0);
-        printf("Fleche du haut\n");
+        *(datas->y) -= 50;
     }
-    
+    else if (key_code == 124)
+    {
+        *(datas->x) += 50;
+    }
+    else if (key_code == 123)
+    {
+        *(datas->x) -= 50;
+    }
     return (0);
 }
 
@@ -92,28 +102,22 @@ int main()
 	t_img	img;
     t_datas datas;
     int     color = create_trgb(0,255,0,0);
-    int     x;
-    int     y;
+    int     x = 10;
+    int     y = 10;
 
     vars.mlx = mlx_init();
     vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Fractol");
 
-	img.img = mlx_new_image(vars.mlx, 1920, 1080);
-	img.first_pix = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	//img.img = mlx_new_image(vars.mlx, 1920, 1080);
+	//img.first_pix = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 
-    datas.vars = vars;
-    datas.img = img;
-
+    datas.vars = &vars;
+    datas.img = &img;
     datas.color = &color;
     datas.x = &x;
     datas.y = &y;
 
 
-    //main_loop(&datas);
-    /*
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-    */
 
     /*PRESS KEY*/
     mlx_key_hook(vars.win, fx_kboard_hook, &datas);
@@ -158,12 +162,64 @@ int on_expose_test (void *param)
     return (0);
 }
 
+
+void    fx_display_square(int size_square, t_datas *datas)
+{
+    
+    my_mlx_pixel_put(datas->img, *(datas->x), *(datas->y), *(datas->color));
+
+    int i;
+    int j;
+    i = 0;
+    while (i <= size_square)
+    {
+        j = 0;
+        while (j <= size_square)
+        {
+            my_mlx_pixel_put(datas->img, *(datas->x)+i, *(datas->y)+j, *(datas->color));
+            j++;
+        }
+        
+        i++;
+    }
+    
+
+
+
+    /*
+    int i;
+    int j;
+    
+    i = *(datas->x);
+
+    while (i <= i + size_square)
+    {
+        j = *(datas->y);
+        while (j <= j + size_square)
+        {
+            if (i >= *(datas->x) && i <= *(datas->x) + size_square)
+            {
+                my_mlx_pixel_put(datas->img, i, j, create_trgb(0,0,255,255));
+            }
+            j++;
+        }
+        i++;
+    }
+    */
+}
+
 int main_loop(t_datas *datas)
 {
+    datas->img->img = mlx_new_image(datas->vars->mlx, 1920, 1080);
+    datas->img->first_pix = mlx_get_data_addr(datas->img->img, 
+                                        &datas->img->bits_per_pixel, 
+                                        &datas->img->line_length,
+                                        &datas->img->endian);
+	//my_mlx_pixel_put(datas->img, *(datas->x), *(datas->y), *(datas->color));
 
+    fx_display_square(50, datas);
 
-	my_mlx_pixel_put(&(datas->img), 5, 5, *(datas->color));
-	mlx_put_image_to_window((datas->vars).mlx, (datas->vars).win, (datas->img).img, 0, 0);
+	mlx_put_image_to_window((datas->vars)->mlx, (datas->vars)->win, (datas->img)->img, 0, 0);
     return (0);
 }
 
