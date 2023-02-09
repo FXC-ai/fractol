0c34a0a307e67cca07325fe8480295fdeabc9fd7
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-02-09 15:27:12 +0100

    able to chage the color of the pixel by hiting up narrow

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "libft/libft.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "mlx.h"
 
 typedef struct s_vars
@@ -23,6 +25,11 @@ typedef struct s_datas
 {
     t_vars  vars;
     t_img   img;
+
+    int     *color;
+    int     *x;
+    int     *y;
+
 }   t_datas;
 
 
@@ -47,24 +54,29 @@ void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-int fx_kboard_hook(int key_code, t_vars *vars)
+int fx_kboard_hook(int key_code, t_datas *datas)
 {
     int x;
     int y;
 
     if (key_code == 53)
     {
-        mlx_destroy_window(vars->mlx, vars->win);
+        mlx_destroy_window(datas->vars.mlx, datas->vars.win);
         exit(0);
     }
-    if (key_code == 0)
+    else if (key_code == 0)
     {
         printf("A appuye\n");
     }
-    if (key_code == 12)
+    else if (key_code == 12)
     {
-        mlx_mouse_get_pos(vars->win , &x, &y);
+        mlx_mouse_get_pos(datas->vars.win , &x, &y);
         printf("x= %d et y= %d\n", x, y);
+    }
+    else if (key_code == 126)
+    {
+        *(datas->color) = create_trgb(0,0,255,0);
+        printf("Fleche du haut\n");
     }
     
     return (0);
@@ -73,10 +85,15 @@ int fx_kboard_hook(int key_code, t_vars *vars)
 int main()
 {
 
+
+
+
     t_vars  vars;
 	t_img	img;
     t_datas datas;
-
+    int     color = create_trgb(0,255,0,0);
+    int     x;
+    int     y;
 
     vars.mlx = mlx_init();
     vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Fractol");
@@ -87,14 +104,19 @@ int main()
     datas.vars = vars;
     datas.img = img;
 
-    main_loop(&datas);
+    datas.color = &color;
+    datas.x = &x;
+    datas.y = &y;
+
+
+    //main_loop(&datas);
     /*
 	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
 	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
     */
 
     /*PRESS KEY*/
-    mlx_key_hook(vars.win, fx_kboard_hook, &vars);
+    mlx_key_hook(vars.win, fx_kboard_hook, &datas);
     /*DESTROY*/
     mlx_hook(vars.win, 17, 0, closed, &vars);
 
@@ -103,6 +125,7 @@ int main()
     /*ON EXPOSE*/
     mlx_hook(vars.win, 12, 0, on_expose_test, &vars);
     
+    mlx_loop_hook(vars.mlx, &main_loop, &datas);
 
 	mlx_loop(vars.mlx);
 
@@ -137,7 +160,9 @@ int on_expose_test (void *param)
 
 int main_loop(t_datas *datas)
 {
-	my_mlx_pixel_put(&(datas->img), 5, 5, 0x00FF0000);
+
+
+	my_mlx_pixel_put(&(datas->img), 5, 5, *(datas->color));
 	mlx_put_image_to_window((datas->vars).mlx, (datas->vars).win, (datas->img).img, 0, 0);
     return (0);
 }
