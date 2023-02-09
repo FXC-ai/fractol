8dda08a132ac49ccdd53f3531abf04f4f96e7c02
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-02-09 14:17:49 +0100

    Safety push

--- main.c
+++ main.c
@@ -2,26 +2,28 @@
 #include <stdio.h>
 #include "mlx.h"
 
-typedef struct s_data
-{
-    void    *img;
-    void    *addr;
-    int     bits_per_pixel;
-    int     line_length;
-    int     endian;
-}   t_data;
-
 typedef struct s_vars
 {
     void    *mlx;
     void    *win;
 }   t_vars;
 
-typedef struct s_coord
+typedef struct s_img
 {
-    int x;
-    int y;
-}   t_coord;
+    void    *img;
+    void    *first_pix;
+
+    int     bits_per_pixel;
+    int     line_length;
+    int     endian;
+}   t_img;
+
+
+typedef struct s_datas
+{
+    t_vars  vars;
+    t_img   img;
+}   t_datas;
 
 
 
@@ -30,18 +32,18 @@ int display_wait(t_vars vars);
 int display_coord(int x, int y, t_vars vars);
 int test (int x, int y, void *param);
 int on_expose_test (void *param);
-
+int main_loop(t_datas *datas);
 
 int	create_trgb(int t, int r, int g, int b)
 {
 	return (t << 24 | r << 16 | g << 8 | b);
 }
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = img->first_pix + (y * img->line_length + x * (img->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
 
@@ -70,38 +72,39 @@ int fx_kboard_hook(int key_code, t_vars *vars)
 
 int main()
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
+
     t_vars  vars;
+	t_img	img;
+    t_datas datas;
 
 
-	mlx = mlx_init();
+    vars.mlx = mlx_init();
+    vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Fractol");
 
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(vars.mlx, 1920, 1080);
+	img.first_pix = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 
-    vars.mlx = mlx;
-    vars.win = mlx_win;
+    datas.vars = vars;
+    datas.img = img;
 
-	img.img = mlx_new_image(mlx, 1920, 1080);
-
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-
+    main_loop(&datas);
+    /*
 	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+    */
 
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
     /*PRESS KEY*/
-    mlx_key_hook(mlx_win, fx_kboard_hook, &vars);
+    mlx_key_hook(vars.win, fx_kboard_hook, &vars);
     /*DESTROY*/
     mlx_hook(vars.win, 17, 0, closed, &vars);
+
     /*MOUSE MOVE*/
     mlx_hook(vars.win, 6, 0, test, &vars);
     /*ON EXPOSE*/
     mlx_hook(vars.win, 12, 0, on_expose_test, &vars);
+    
 
-
-	mlx_loop(mlx);
+	mlx_loop(vars.mlx);
 
     return (0);
 }
@@ -129,6 +132,13 @@ int on_expose_test (void *param)
 
     printf("ON_EXPOSE active\n");
 
+    return (0);
+}
+
+int main_loop(t_datas *datas)
+{
+	my_mlx_pixel_put(&(datas->img), 5, 5, 0x00FF0000);
+	mlx_put_image_to_window((datas->vars).mlx, (datas->vars).win, (datas->img).img, 0, 0);
     return (0);
 }
 
